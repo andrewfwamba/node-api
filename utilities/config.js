8db5978da70f91ad378084c197bb2e6fa6a6f36c
Andrew Fwamba <andrewecorrex03@gmail.com>
2024-02-15 21:37:22 +0300

    task: configurations

---
+++ utilities/config.js
@@ -0,0 +1,12 @@
+export default {
+  development: {
+    username: "root",
+    password: "",
+    database: "nodetest",
+    host: "localhost",
+    dialect: "mysql",
+  },
+  production: {
+    // Production configuration
+  },
+};
