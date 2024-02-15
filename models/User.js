72feb2b59c05ad66b33ffa8cab31d4dbbeab1acd
Andrew Fwamba <andrewecorrex03@gmail.com>
2024-02-15 21:36:04 +0300

    task: user modle

---
+++ models/User.js
@@ -0,0 +1,39 @@
+// models/User.js
+import { DataTypes } from "sequelize";
+import bcrypt from "bcrypt";
+import sequelize from "../services/db.js";
+
+const User = sequelize.define(
+  "User",
+  {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (user) => {
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(user.password, saltRounds);
+        user.password = hashedPassword;
+      },
+    },
+  }
+);
+
+User.prototype.isValidPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export default User;
