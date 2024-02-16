e06ad1ea67075d11e2999f2270b357a0c7344f22
Andrew Fwamba <andrewecorrex03@gmail.com>
2024-02-16 22:29:34 +0300

    task: cleanup

--- services/db.js
+++ services/db.js
@@ -1,6 +1,5 @@
 import Sequelize from "sequelize";
 import config from "../utilities/config.js";
-// import config from "./config.js";
 
 const env = process.env.NODE_ENV || "development";
 const { database, username, password, ...rest } = config[env];
