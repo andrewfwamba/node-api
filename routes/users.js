a8427486cfe675fe8a66989a19a8f30e6d68529f
Andrew Fwamba <andrewecorrex03@gmail.com>
2024-02-16 22:26:37 +0300

    task: fetch user details

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createUser,
   getAllUsers,
+  getUser,
   signInUser,
 } from "../controllers/userController.js";
 import { isAuth } from "../middlewares/authorization/userAuth.js";
@@ -12,5 +13,6 @@ router.post("/signup", createUser);
 router.post("/signin", signInUser);
 // Authorize route
 router.get("/users", isAuth, getAllUsers);
+router.get("/userdetails", isAuth, getUser);
 
 export default router;
