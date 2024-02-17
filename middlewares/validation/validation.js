a08ad72e70f9e74bbd323989df737ef8cb487bc0
Andrew Fwamba <andrewecorrex03@gmail.com>
2024-02-17 21:59:26 +0300

    task: validation middleware

---
+++ middlewares/validation/validation.js
@@ -0,0 +1,39 @@
+import { check, validationResult } from "express-validator";
+
+exports.validateSignup = [
+  check("username")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("username is a mandatory field")
+    .isString()
+    .withMessage("invalid username"),
+  // check email
+  check("email")
+    .normalizeEmail()
+    .isEmail()
+    .withMessage("Invalid email address"),
+  // check password values
+  check("password")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("Password cannot be empty")
+    .isLength({ min: 8, max: 20 })
+    .withMessage("Password must between 8 and 20 characters long"),
+];
+
+exports.validateUserSignIn = [
+  check("email").trim().isEmail().withMessage("email/password is required"),
+  check("password")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("email/password is required"),
+];
+exports.userValidation = (req, res, next) => {
+  const result = validationResult(req).array();
+  if (!result.length) return next();
+  const error = result[0].msg;
+  res.json({ success: false, message: error });
+};
