827412d53ba6f385b3759de5ba3a3bdd695fb62e
Andrew Fwamba <andrewecorrex03@gmail.com>
2024-02-26 22:37:44 +0300

    feat: reset pass routes

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 import express from "express";
 import {
+  confirmPasswordReset,
   createUser,
   getAllUsers,
   getUser,
+  initiatePasswordReset,
   signInUser,
 } from "../controllers/userController.js";
 import { isAuth } from "../middlewares/authorization/userAuth.js";
@@ -10,6 +12,8 @@ import {
   validateSignup,
   userValidation,
   validateUserSignIn,
+  initResetpassword,
+  resetPassValidation,
 } from "../middlewares/validation/validation.js";
 
 const router = express.Router();
@@ -19,5 +23,18 @@ router.post("/signin", validateUserSignIn, userValidation, signInUser);
 // Authorize route
 router.get("/users", isAuth, getAllUsers);
 router.get("/userdetails", isAuth, getUser);
+// reset pass routes
+router.post(
+  "/reset-password",
+  initResetpassword,
+  userValidation,
+  initiatePasswordReset
+);
+router.post(
+  "/reset-password/confirm",
+  resetPassValidation,
+  userValidation,
+  confirmPasswordReset
+);
 
 export default router;
